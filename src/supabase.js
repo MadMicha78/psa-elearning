a6479b27e285de1deac181a58025c7423b2efe92
MadMicha78 <info@psarbeitssicherheit.de>
2026-04-20 17:05:29 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://vjyztwsqhnpwbwehzlob.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_nQ63u3trB59SQvaXhi5evg_19pHbu-F'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqeXp0d3NxaG5wd2J3ZWh6bG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2ODg1MjEsImV4cCI6MjA5MjI2NDUyMX0.Ph4MAja7xxhOFCWWBZ4bC2cLAckSnBC6mf6VkacIEIg'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
