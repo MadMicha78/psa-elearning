13e74a1f6d22beac4768cc2708f29c45f302df58
MadMicha78 <info@psarbeitssicherheit.de>
2026-05-21 12:58:27 +0000

    Phase 3C: TenantBadge aus main.jsx entfernt

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { detectTenantId } from './tenants/useTenant'
-import { TenantBadge } from './tenants/TenantBadge'
 import { WissenApp } from './tenants/WissenApp'
 
 const tenantId = detectTenantId()
@@ -12,6 +11,5 @@ console.log('🎯 Aktueller Tenant:', tenantId)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {tenantId === 'wissen' ? <WissenApp /> : <App />}
-    <TenantBadge />
   </React.StrictMode>
 )
